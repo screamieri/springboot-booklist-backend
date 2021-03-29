e30bf9f82c8ceb3e225f1d7688641eb5097bafa4
Andrea Scrimieri <74497321+screamieri@users.noreply.github.com>
2021-03-29 22:43:57 +0200

    Updated test environment

--- src/test/java/com/andreascrimieri/bookapp/BookApp/BookServiceTest.java
+++ src/test/java/com/andreascrimieri/bookapp/BookApp/BookServiceTest.java
@@ -1,8 +1,6 @@
 package com.andreascrimieri.bookapp.BookApp;
 
 import com.andreascrimieri.bookapp.BookApp.service.BookService;
-import com.andreascrimieri.bookapp.BookApp.service.UserService;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,13 +20,5 @@ class BookServiceTest {
 	@Autowired
 	BookService bookService;
 
-	@Autowired
-	UserService userService;
-
-
-	@Test
-	public void shouldFetchAllBooks() throws Exception{
-
-	}
 
 }
